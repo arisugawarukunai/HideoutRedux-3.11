a0ee04f19d374e178f2fd1d3fdb0701ff467cf58
arisugawarukunai <129881470+arisugawarukunai@users.noreply.github.com>
2025-05-24 16:09:12 -0300

    Merge: release v1.1.3
    * 1.1.3a: Updated "types" backend to SPT 3.11
    JSONC config was made into a regular JSON for simplicity in importing
    ScavCase and Prod recipes deactivated due to backend changes
    
    * 1.1.3b: Added back changes to production and scav cases
    "scavcase.json" fields edited to better cast to IScavRecipe type

--- types/models/eft/match/IGetRaidConfigurationRequestData.d.ts
+++ types/models/eft/match/IGetRaidConfigurationRequestData.d.ts
@@ -1,6 +1,6 @@
 import { IRaidSettings } from "@spt/models/eft/match/IRaidSettings";
 export interface IGetRaidConfigurationRequestData extends IRaidSettings {
-    keyId: string;
-    CanShowGroupPreview: boolean;
+    onlinePveRaidStates: Record<string, boolean>;
+    transitionType: number;
     MaxGroupCount: number;
 }
